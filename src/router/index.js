d51eb286350f08468290311a9abdf8981a7e0b29
Jeosafá Ferreira do Nascimento <jeosafa.jfn@isgh.org.br>
2023-03-13 17:08:30 -0300

    AdminLTE funcionando, dashboard ok.

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,22 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/Login.vue'
-import Home from '@/views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login.vue";
+import Home from "@/views/Home.vue";
 
 const routes = [
-  {
-    path: '',
-    name: 'Login',
-    component: Login,
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  }
-]
+    {
+        path: "",
+        component: Login,
+    },
+    {
+        path: "/home",
+        component: Home,
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
